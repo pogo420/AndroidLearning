2a63753ec4b8ae256e878f9045313536c6f2b2e9
pogo420 <am0072008@gmail.com>
2021-01-02 09:41:53 +0530

    App update

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -3,18 +3,14 @@ package com.example.androidlearning;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
-import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_testing);
+        setContentView(R.layout.activity_1_liner);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
