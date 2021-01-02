681fd045391d40c5c34ba46504c53ae75f3fe9d5
pogo420 <am0072008@gmail.com>
2021-01-02 21:55:25 +0530

    Order app setup

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -1,61 +1,55 @@
 package com.example.androidlearning;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.util.Log;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     String tag = "SampleDemo:";
-//    TextView textView;
+    int globalCount = 1;
+    int priceAnount = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.userinput_test);
         setContentView(R.layout.userinput_test);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         Log.i(tag, "--onCreate--");
-//        textView = findViewById(R.id.textView);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void increaseOrder(View view){
+        globalCount++;
+        displayQuantity(globalCount);
+//        displayPrice(priceAnount*globalCount);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void decreaseOrder(View view){
+        globalCount--;
+        displayQuantity(globalCount);
+//        displayPrice(priceAnount*globalCount);
+    }
+    public void submitOrder(View view){
+        displayPrice(priceAnount*globalCount);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    public void reset(View view){
+        globalCount=0;
+        displayQuantity(globalCount);
+        displayPrice(priceAnount*globalCount);
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void displayQuantity(int value){
+        TextView quantitity = (TextView) findViewById(R.id.quantity_value);
+        quantitity.setText("" + value);
+    }
+    private void displayPrice(int value){
+        TextView quantitity = (TextView) findViewById(R.id.price_value);
+
+        String message = String.format("Total Price INR %d\nThnak you!!", value);
+        quantitity.setText(message);
     }
 
     @Override
@@ -93,8 +87,4 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         Log.i(tag, "--onPause--");
     }
-//    public void clickHandler(View view){
-//        textView.setText("Hello Android");
-//        Toast.makeText(this, "Clicked a button",Toast.LENGTH_LONG).show();
-//    }
 }
