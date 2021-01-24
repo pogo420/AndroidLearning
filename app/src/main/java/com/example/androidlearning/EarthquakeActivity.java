af7548fc5119d2a02dc1e7b3fa3eff108be2ba57
pogo420 <am0072008@gmail.com>
2021-01-24 10:22:11 +0530

    API call updates

--- app/src/main/java/com/example/androidlearning/EarthquakeActivity.java
+++ app/src/main/java/com/example/androidlearning/EarthquakeActivity.java
@@ -1,14 +1,23 @@
 package com.example.androidlearning;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
 public class EarthquakeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +42,32 @@ public class EarthquakeActivity extends AppCompatActivity {
 //        earthquakes.add(new QuakeData("Kolkata", "25-02-2021", 5.90));
 //        earthquakes.add(new QuakeData("Kolkata", "25-02-2021", 5.90));
 
-        ArrayList<QuakeData> earthquakes = QueryUtil.extractEarthquakes();
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute();
+//        ArrayList<QuakeData> earthquakes = QueryUtil.extractEarthquakes();
+
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<Void, Void, ArrayList<QuakeData> > {
+        // inner class for async operation will be depcicated in future.
+
+        @Override
+        protected ArrayList<QuakeData>  doInBackground(Void... voids) {
+            ArrayList<QuakeData>  result = QueryUtil.extractEarthquakes();
+            return result;
+        }
+
+        protected void onPostExecute(ArrayList<QuakeData> result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+
+            updateAdapter(result);
+        }
+    }
+
+    private void updateAdapter(ArrayList<QuakeData> earthquakes) {
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
@@ -48,6 +82,15 @@ public class EarthquakeActivity extends AppCompatActivity {
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
 
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Get the {@link Word} object at the given position the user clicked on
+                QuakeData quakeData = earthquakes.get(position);
+                System.out.println(quakeData.getCity());
+            }
+        });
+
     }
-    
+
 }
--- app/src/main/java/com/example/androidlearning/QuakeAdapter.java
+++ app/src/main/java/com/example/androidlearning/QuakeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.androidlearning;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,46 @@ public class QuakeAdapter extends ArrayAdapter<QuakeData> {
         // we are manually adding data to items so we wont set it.
     }
 
+    private int getColorMagnitude(double magnitude){
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return magnitudeColorResourceId;
+    }
+
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -32,12 +73,21 @@ public class QuakeAdapter extends ArrayAdapter<QuakeData> {
         // getting current value
         QuakeData currentQuake = getItem(position); // each data
 
+        // View 1
         TextView text1 = listItemView.findViewById(R.id.magnitude);
         text1.setText(String.valueOf(currentQuake.getMagnitude()));
+        // Getting the background
+        GradientDrawable magnitudeCircle = (GradientDrawable) text1.getBackground();
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getColorMagnitude(currentQuake.getMagnitude());
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
 
+        // View 2
         TextView text2 = listItemView.findViewById(R.id.city);
         text2.setText(currentQuake.getCity());
 
+        // View 3
         TextView text3 = listItemView.findViewById(R.id.date);
         text3.setText(currentQuake.getDate());
 
